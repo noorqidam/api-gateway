2168c011287b75bcb59bd122180fad52295d677f
noorqidam <noorqidam@gmail.com>
2021-03-01 09:53:01 +0700

    fix typo URL_SERVICE

--- routes/handler/media/getAll.js
+++ routes/handler/media/getAll.js
@@ -1,10 +1,19 @@
 const apiAdapter = require("../../apiAdapter");
-const { URL_SERVICE_DATA } = process.env;
-const api = apiAdapter(URL_SERVICE_DATA);
+const { URL_SERVICE_MEDIA } = process.env;
+const api = apiAdapter(URL_SERVICE_MEDIA);
 
 module.exports = async (req, res) => {
   try {
     const media = await api.get("/media", req.body);
     return res.json(media.data);
-  } catch (error) {}
+  } catch (error) {
+    if (error.code === "ECONNREFUSED") {
+      return res
+        .status(500)
+        .json({ status: "error", message: "service unavailable" });
+    }
+
+    const { status, data } = error.response;
+    return res.status(status).json(data);
+  }
 };

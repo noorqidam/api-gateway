a189fbdd0502a272c673ee7c0d822f77589060fd
noorqidam <noorqidam@gmail.com>
2021-03-02 15:00:47 +0700

    implementation API update user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const verifyToken = require("../middlewares/verifyToken");
+const router = express.Router();
 const userHandler = require("./handler/users");
 
 router.post("/register", userHandler.register);
 router.post("/login", userHandler.login);
+router.put("/", verifyToken, userHandler.update);
 
 module.exports = router;
